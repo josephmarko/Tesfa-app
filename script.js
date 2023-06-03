a9b1e81e2e7b4d2f18e8cea3b1538d51144baa1d
josephmarko <josephmmakaro@gmail.com>
2023-06-03 23:43:23 +0200

    Done login

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const emailEl = document.getElementsByName('email');
+const passwordEl = document.getElementsByName('password');
+const formEl = document.getElementsByClassName('login');
